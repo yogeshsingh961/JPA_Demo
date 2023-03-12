b05103a6e89e72d65e54ebe1ab781752207f5622
Yogesh Singh <yogeshsingh3596@gmail.com>
2023-03-12 10:49:15 +0530

    delete api created

--- src/main/java/com/AkhandBharat/jpademo/UserController.java
+++ src/main/java/com/AkhandBharat/jpademo/UserController.java
@@ -1,10 +1,9 @@
 package com.AkhandBharat.jpademo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -15,9 +14,40 @@ public class UserController {
     public String addUser(@RequestBody User user){
        return userService.addUser(user);
     }
+
+    @GetMapping("get_users")
+    public List<User> getUsers(){
+       return userService.getUsers();
+    }
+
+    @GetMapping("/get_userById")
+    public User getUsers(@RequestParam("q") int id){
+        return  userService.getAUser(id);
+    }
+
     // H.W.
     // delete by a rollNo
     // delete all the users
     // update a user by id
+
+
+
+    @DeleteMapping("/deleteByRollNo")
+    public String deleteByRollNo(@RequestParam int id){
+      return userService.deleteByRollNo(id);
+
+    }
+
+    @DeleteMapping("/deleteUsers")
+    public String deleteUsers(){
+       return userService.deleteUsers();
+
+    }
+
+//    @PutMapping("updateById")
+//    public String updateUserById(@RequestParam int id){
+//       return userService.updateUserById(id);
+//
+//    }
 
 }
--- src/main/java/com/AkhandBharat/jpademo/UserService.java
+++ src/main/java/com/AkhandBharat/jpademo/UserService.java
@@ -3,6 +3,8 @@ package com.AkhandBharat.jpademo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 @Autowired
@@ -12,5 +14,34 @@ public class UserService {
         userRepository.save(user);
         return "User added";
     }
+
+
+    public List<User> getUsers(){
+        return  userRepository.findAll();
+    }
+
+
+    public User getAUser(int id){
+        return  userRepository.findById(id).get();
+    }
+
+    public String deleteByRollNo(int id){
+        userRepository.deleteById(id);
+        return "User deleted";
+    }
+
+
+
+    public String deleteUsers(){
+        userRepository.deleteAll();
+        return "All users have been deleted";
+
+    }
+
+//    public String updateUserById(int id){
+//        userRepository.;
+//        return "User updated";
+//
+//    }
 
 }
